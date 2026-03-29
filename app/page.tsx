cd2ece087de45a7d2d6310d930cb76894818fb08
ChinmayaBisoi <bisoi.chinmaya.1999@gmail.com>
2026-03-29 22:10:22 +0530

    remove landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-import { LandingFeatures } from "@/components/landing/LandingFeatures";
-import { LandingHero } from "@/components/landing/LandingHero";
+
 
 export default function Home() {
   return (
-    <div className="min-h-dvh bg-background font-sans text-foreground">
-      <div className="flex min-h-dvh w-full flex-col sm:p-2 sm:pl-0">
-        <div className="flex min-h-dvh w-full flex-col overflow-y-auto rounded-b-2xl bg-card shadow-sm">
-          <main className="flex w-full flex-col items-center justify-center">
-            <LandingHero />
-            <LandingFeatures />
-          </main>
-        </div>
-      </div>
+    <div>
+      {/* Hi */}
     </div>
   );
 }
